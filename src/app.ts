c4e175eeaa6b2ce8d69fe10ebcd3b0da95bd27bb
aanishtahir19 <aanishtahir19@gmail.com>
2021-12-18 17:38:18 +0500

    Added files for boilerplate

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 const app = express();
+declare let process : {
+    env: {
+      PORT: number
+    }
+  }
+const PORT= process.env.PORT | 3000;
+app.use(express.json());
 
 
-app.listen(4000, ()=> console.log("Server Running..."))
+
+app.use((req:Request, res:Response)=>{
+    res.status(404).send("Invalid URL")
+})
+
+app.listen(PORT, ()=> console.log("Server Running..."))
